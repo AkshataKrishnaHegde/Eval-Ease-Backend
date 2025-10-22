f2782ad3945a891b33d54a4797a384410ac7043a
AkshataKrishnaHegde <hegdeakshata478@gmail.com>
2025-10-22 20:38:29 +0530

    commit

---
+++ src/main/java/com/evalease/evalease_backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.evalease.evalease_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "EvalEase Backend is running successfully!";
+    }
+}
